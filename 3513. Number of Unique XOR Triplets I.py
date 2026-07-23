10e169f5e1a79859bf888608e735bcb53c812248
Suryaa-k <suryaanish1149198@gmail.com>
2026-07-23 16:43:53 +0530

    3513

---
+++ 3513. Number of Unique XOR Triplets I.py
@@ -0,0 +1,11 @@
+class Solution:
+    def uniqueXorTriplets(self, nums: list[int]) -> int:
+        n = len(nums)
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        p = 1
+        while p <= n:
+            p <<= 1
+        return p
