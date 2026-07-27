93bb824cbbe0a9cb115986a1d17e073127d518d5
Suryaa-k <suryaanish1149198@gmail.com>
2026-07-27 20:59:08 +0530

    1464

---
+++ 1464. Maximum Product of Two Elements in an Array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxProduct(self, nums: list[int]) -> int:
+        first = second = 0
+        for x in nums:
+            if x >= first:
+                second = first
+                first = x
+            elif x > second:
+                second = x
+        return (first - 1) * (second - 1)
